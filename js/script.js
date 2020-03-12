9621282e7d1c3a8924c4d10855bd21036efe8406
zoemclean <zoemclean101@gmail.com>
2020-03-13 09:32:43 +1300

    intro to jquery

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// alert ('javascript has loaded')
+//$
+//jQuery
+
+// $(selector).effect()
+
+
+
+$('#blueBox').hide();
+
+$('#greenBox').click(function(){
+    $('#blueBox').show();
+})
+
+$('#redBox').click(function(){
+    $('#blueBox').hide();
+})
