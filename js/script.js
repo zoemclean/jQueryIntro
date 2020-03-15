1ceaba6c3e72808bb192e021ca50c7125708d2db
zoemclean <zoemclean101@gmail.com>
2020-03-16 10:07:19 +1300

    learning about toggles anf nav

--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@
 //jQuery
 
 // $(selector).effect()
+            // .action()
+            // .event()
 
 
 
@@ -10,8 +12,25 @@ $('#blueBox').hide();
 
 $('#greenBox').click(function(){
     $('#blueBox').show();
-})
+});
 
 $('#redBox').click(function(){
     $('#blueBox').hide();
+});
+
+$('#modalExample1Button').click(function(){
+    // console.log('button has been clicked');
+    $('#modalExample1').removeClass('hidden').addClass('flex');
+    $('body').addClass('noScroll');
+});
+
+$('#modalExample1Close').click(function(){
+    // console.log('button has been clicked');
+    $('#modalExample1').addClass('hidden').removeClass('flex');
+    $('body').removeClass('noScroll');
+})
+
+$('#navListToggleButton').click(function(){
+    // $('#navList').toggle();
+    $('#navList').slidetoggle(300);
 })
